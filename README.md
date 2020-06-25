e98f00501c6d22297cd07603b7a71ee339d26fe2
xiejt <xiejt@dsp.ac.cn>
2020-06-25 10:42:06 +0800

    add cookie

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
 ## Code Community
 
 ## 资料
-[Spring 文档](https://spring.io/guides)
-
-[Spring Web](https://spring.io/guides/gs/serving-web-content/)
-
-[es社区](https://elasticsearch.cn/explore)
-
-[Bootstrap](https://v3.bootcss.com/)
-
-[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
-
-[OkHttp](https://square.github.io/okhttp/)
+<br>[Spring 文档](https://spring.io/guides)
+<br>[Spring Web](https://spring.io/guides/gs/serving-web-content/)
+<br>[es社区](https://elasticsearch.cn/explore)
+<br>[Bootstrap](https://v3.bootcss.com/)
+<br>[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+<br>[OkHttp](https://square.github.io/okhttp/)
+<br>[SPringBoot Features SQL](https://docs.spring.io/spring-boot/docs/2.1.13.RELEASE/reference/html/boot-features-sql.html)
+<br>[Mybatis SpringBoot Autoconfigure](https://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/)
 
 ## 工具
-git
+<br>git
+<br>[Visual Paradigm](www.visual-paradigm.com)
 
-[Visual Paradigm](www.visual-paradigm.com)
-
-
+## 脚本
+```mysql-sql
+CREATE TABLE user
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	account_id varchar(100),
+	name VARCHAR(50),
+	token CHAR(36),
+	gmt_create BIGINT,
+	gmt_modified BIGINT
+);
+```
--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -2,16 +2,24 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.AccessTokenDTO;
 import com.example.demo.dto.GithubUser;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.User;
 import com.example.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+<<<<<<< HEAD
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+=======
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+>>>>>>> 08346e8f11e9f1e29b31ddba281ee07fa187fc08
 
 @Controller
 public class AuthorizeController {
@@ -26,10 +34,18 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
+<<<<<<< HEAD
+                           HttpServletRequest request,
+                           HttpServletResponse response){
+=======
                            HttpServletRequest request){
+>>>>>>> 08346e8f11e9f1e29b31ddba281ee07fa187fc08
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -37,10 +53,26 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+<<<<<<< HEAD
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            Cookie cookie = new Cookie("token", token);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+=======
         GithubUser user = githubProvider.getUser(accessToken);
         if(user != null){
             // 登陆成功 写cookie和session
             request.getSession().setAttribute("user", user);
+>>>>>>> 08346e8f11e9f1e29b31ddba281ee07fa187fc08
             return "redirect:/";
         }else{
             // 登录失败 重新登录
