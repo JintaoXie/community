7acaa5807210c17ed0d6ce306f163ff00af2ca91
xiejt <xiejt@dsp.ac.cn>
2020-06-26 15:31:08 +0800

    add publish function

---
+++ src/main/java/com/example/demo/mapper/QuestionMapper.java
@@ -0,0 +1,15 @@
+package com.example.demo.mapper;
+
+import com.example.demo.model.Question;
+import com.example.demo.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (title, description, gmt_create, gmt_modified, creator, tag) values (#{title}, #{description}, #{gmtCreate}, #{gmtModified}, #{creator}, #{tag})")
+    void create(Question question);
+
+}
